46787492425ade851ddfec834dc35f634ac81398
root <15939557197@163.com>
2018-05-06 20:48:03 +0800

    少加了个注解,导致html页面加载静态资源找不到

--- xunwu/src/main/java/com/luqi/config/WebMvcConfig.java
+++ xunwu/src/main/java/com/luqi/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
@@ -16,6 +17,7 @@ import org.thymeleaf.spring4.view.ThymeleafViewResolver;
  * Created by luQi
  * 2018-05-06 12:31.
  */
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
