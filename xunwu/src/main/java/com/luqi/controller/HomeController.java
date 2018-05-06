19139719fd74cdb23fffd386fc383acdc59ecb85
root <15939557197@163.com>
2018-05-06 13:02:19 +0800

    集成Thymeleaf

---
+++ xunwu/src/main/java/com/luqi/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.luqi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by luqi
+ * 2018-05-06 12:51.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "luqi");
+        return  "index";
+    }
+}
