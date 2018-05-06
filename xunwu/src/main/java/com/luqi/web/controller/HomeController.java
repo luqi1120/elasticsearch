393f287b5843cdcc0e6ccb1ccf19fca1d9bc5274
root <15939557197@163.com>
2018-05-06 15:52:51 +0800

    自定义错误页面

--- xunwu/src/main/java/com/luqi/web/controller/HomeController.java
+++ xunwu/src/main/java/com/luqi/web/controller/HomeController.java
@@ -16,4 +16,24 @@ public class HomeController {
         model.addAttribute("name", "luqi");
         return  "index";
     }
+
+    @GetMapping("/404")
+    public String notFoundPage() {
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError() {
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError() {
+        return "500";
+    }
+
+    @GetMapping("/logout/page")
+    public String logoutPage() {
+        return "logout";
+    }
 }
