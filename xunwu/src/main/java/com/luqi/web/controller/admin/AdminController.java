aedf2967663625f28474b1ac8c504183d3570366
root <15939557197@163.com>
2018-05-06 21:23:30 +0800

    文件上传

--- xunwu/src/main/java/com/luqi/web/controller/admin/AdminController.java
+++ xunwu/src/main/java/com/luqi/web/controller/admin/AdminController.java
@@ -1,7 +1,13 @@
 package com.luqi.web.controller.admin;
 
+import com.luqi.base.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Created by luqi
@@ -35,5 +41,24 @@ public class AdminController {
     @GetMapping("/admin/login")
     public String adminLoginPage() {
         return "admin/login";
+    }
+
+    /**
+     * 添加房源页
+     * @return
+     */
+    @GetMapping("/admin/add/house")
+    public String addHousePage() {
+        return "admin/house-add";
+    }
+
+    /**
+     * 上传图片
+     * @return
+     */
+    @PostMapping(value = "/admin/update/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse updatePhoto(@RequestParam("file") MultipartFile file) {
+        return ApiResponse.ofSuccess(null);
     }
 }
