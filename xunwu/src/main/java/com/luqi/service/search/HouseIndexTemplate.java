96b62020f353b2b864bd03d6378cab04f27f582e
root <15939557197@163.com>
2018-05-27 17:16:37 +0800

    ES对应的实体类

---
+++ xunwu/src/main/java/com/luqi/service/search/HouseIndexTemplate.java
@@ -0,0 +1,217 @@
+package com.luqi.service.search;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 索引结构模板
+ * Created by luQi
+ * 2018-05-27 13:55.
+ */
+public class HouseIndexTemplate {
+
+    private Long houseId;
+
+    private String title; // 标题
+
+    private int price;
+
+    private int area;
+
+    private Date createTime; // 排序
+
+    private Date lastUpdateTime; // 默认排序
+
+    private String cityEnName; // 城市
+
+    private String regionEnName; // 地区
+
+    private int direction; // 朝向(方向东西南北设置的是1234所以用integer表示)
+
+    private int distanceToSubway; // 与地铁的距离
+
+    private String subwayLineName; // 地铁站名
+
+    private String subwayStationName; // 地铁线路
+
+    private String street; // 街道
+
+    private String district; // 小区
+
+    private String description; // 房屋描述信息
+
+    private String layoutDesc; // 布局
+
+    private String traffic; // 交通状况(公交)
+
+    private String roundService; // 周边配套设置
+
+    private int rentWay; // 租住方式(整租,合租)
+
+    private List<String> tags; // 标签 ES中适用的是test 相当于string这里用list表示
+
+//    private List<HouseSuggest> suggest;
+//
+//    private BaiduMapLocation location;
+
+
+    public Long getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(Long houseId) {
+        this.houseId = houseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public String getCityEnName() {
+        return cityEnName;
+    }
+
+    public void setCityEnName(String cityEnName) {
+        this.cityEnName = cityEnName;
+    }
+
+    public String getRegionEnName() {
+        return regionEnName;
+    }
+
+    public void setRegionEnName(String regionEnName) {
+        this.regionEnName = regionEnName;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public int getDistanceToSubway() {
+        return distanceToSubway;
+    }
+
+    public void setDistanceToSubway(int distanceToSubway) {
+        this.distanceToSubway = distanceToSubway;
+    }
+
+    public String getSubwayLineName() {
+        return subwayLineName;
+    }
+
+    public void setSubwayLineName(String subwayLineName) {
+        this.subwayLineName = subwayLineName;
+    }
+
+    public String getSubwayStationName() {
+        return subwayStationName;
+    }
+
+    public void setSubwayStationName(String subwayStationName) {
+        this.subwayStationName = subwayStationName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLayoutDesc() {
+        return layoutDesc;
+    }
+
+    public void setLayoutDesc(String layoutDesc) {
+        this.layoutDesc = layoutDesc;
+    }
+
+    public String getTraffic() {
+        return traffic;
+    }
+
+    public void setTraffic(String traffic) {
+        this.traffic = traffic;
+    }
+
+    public String getRoundService() {
+        return roundService;
+    }
+
+    public void setRoundService(String roundService) {
+        this.roundService = roundService;
+    }
+
+    public int getRentWay() {
+        return rentWay;
+    }
+
+    public void setRentWay(int rentWay) {
+        this.rentWay = rentWay;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
