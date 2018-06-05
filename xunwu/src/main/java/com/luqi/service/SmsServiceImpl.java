ea2727a4ff642f9d8fd1bc2d1191cb048c2e197b
root <15939557197@163.com>
2018-06-05 22:45:10 +0800

    阿里云实现免密码登录

--- xunwu/src/main/java/com/luqi/service/SmsServiceImpl.java
+++ xunwu/src/main/java/com/luqi/service/SmsServiceImpl.java
@@ -1,27 +1,142 @@
 package com.luqi.service;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by luqi
  * 2018-06-04 21:55.
  */
 @Service
-public class SmsServiceImpl implements SmsService/*, InitializingBean*/ {
+public class SmsServiceImpl implements SmsService, InitializingBean {
+
+    @Value("${aliyun.sms.accessKey}")
+    private String accessKey;
+
+    @Value("${aliyun.sms.accessKeySecret}")
+    private String secertKey;
+
+    @Value("${aliyun.sms.template.code}")
+    private String templateCode;
+
+    // 初始化客户端
+    private IAcsClient acsClient;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    private final static String SMS_CODE_CONTENT_PREFIX = "SMS::CODE::CONTENT";
+
+    private static final String[] NUMS = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    private static final Random random = new Random();
 
     @Override
     public ServiceResult<String> sendSms(String telephone) {
-        return ServiceResult.of("123456");
+        String gapKey = "SMS::CODE::INTERVAL::" + telephone;
+
+        // 从redis中获取验证码
+        String result = redisTemplate.opsForValue().get(gapKey);
+        if (result != null) {
+            // 1分钟内 如果redis中有验证码
+            return new ServiceResult<String>(false, "请求次数太频繁");
+        }
+
+        String code = generateRandomSmsCode();
+
+        // 发送短信 套用短信模板
+        String templateParam = String.format("{\"code\": \"%s\"}", code);
+
+        // 组装请求对象
+        SendSmsRequest request = new SendSmsRequest();
+
+        // 使用post提交
+        request.setMethod(MethodType.POST);
+        request.setPhoneNumbers(telephone);
+        request.setTemplateParam(templateParam);
+        request.setTemplateCode(templateCode);
+        request.setSignName("测试");
+
+        Boolean success = false;
+
+        try {
+            SendSmsResponse response = acsClient.getAcsResponse(request);
+            if ("OK".equals(response.getCode())) {
+                success = true;
+            } else {
+                // TODO log this question
+            }
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+        if (success) {
+
+            // 验证码发送成功,1分钟之内不能重复点击,为了让前台验证码框变成不能点击状态
+            redisTemplate.opsForValue().set(gapKey, code, 60, TimeUnit.SECONDS);
+
+            // 如果发送成功保存在redis中
+            redisTemplate.opsForValue().set(SMS_CODE_CONTENT_PREFIX + telephone, code, 10, TimeUnit.MINUTES);
+
+            return ServiceResult.of(code);
+        } else {
+            // 如果发送短信失败
+            return new ServiceResult<String>(false, "服务忙,请稍后重试");
+        }
     }
 
     @Override
     public String getSmsCode(String telephone) {
-        return "123456";
+
+        // 直接从redis中获取验证码
+        return this.redisTemplate.opsForValue().get(SMS_CODE_CONTENT_PREFIX + telephone);
     }
 
     @Override
     public void remove(String telephone) {
 
+        // 删除redis中验证码
+        this.redisTemplate.delete(SMS_CODE_CONTENT_PREFIX + telephone);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 设置超时时间
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKey, secertKey);
+
+        // 固定的不需要改变
+        String product = "Dysmsapi";
+        String domain = "dysmsapi.aliyuncs.com";
+
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+        this.acsClient = new DefaultAcsClient(profile);
+    }
+
+    /**
+     * 6位验证码生成器
+     * @return
+     */
+    private static String generateRandomSmsCode() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(10);
+            sb.append(NUMS[index]);
+        }
+        return sb.toString();
     }
 }
--- xunwu/src/main/java/com/luqi/web/controller/HomeController.java
+++ xunwu/src/main/java/com/luqi/web/controller/HomeController.java
@@ -1,8 +1,16 @@
 package com.luqi.web.controller;
 
+import com.luqi.base.ApiResponse;
+import com.luqi.base.LoginUserUtil;
+import com.luqi.service.ServiceResult;
+import com.luqi.service.SmsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by luqi
@@ -10,6 +18,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class HomeController {
+
+
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping(value = {"/", "/index"}) // 兼容
     public String index(Model model) {
@@ -35,5 +47,27 @@ public class HomeController {
     @GetMapping("/logout/page")
     public String logoutPage() {
         return "logout";
+    }
+
+    /**
+     * 获取验证码
+     * @param telephone
+     * @return
+     */
+    @GetMapping(value = "sms/code")
+    @ResponseBody
+    public ApiResponse smsCode(@RequestParam("telephone") String telephone) {
+
+        // 检测手机号是否合法
+        if (!LoginUserUtil.checkTelephone(telephone)) {
+            return ApiResponse.ofMessage(HttpStatus.BAD_REQUEST.value(), "请输入正确的手机号");
+        }
+        ServiceResult<String> result = smsService.sendSms(telephone);
+        if (result.isSuccess()) {
+            return ApiResponse.ofSuccess("");
+        } else {
+            return ApiResponse.ofMessage(HttpStatus.BAD_REQUEST.value(), result.getMessage());
+        }
+
     }
 }
